77f106e6043a8b13310fc5345d80853bb03b3f7c
Devon Peters <Devon.Peters18@kzoo.edu>
2022-06-29 10:15:56 -0400

    Added a function to try and catch user input to see if it's valid (returns a 1 or 2).

--- src/Dragon_Main.java
+++ src/Dragon_Main.java
@@ -8,12 +8,14 @@ public class Dragon_Main {
            "and will share his treasure with you. The other dragon \n"+
             "is hungry and greedy and will eat you on sight. \n"+
             "Which cave will you go into (1 or 2)?");
-        Scanner derf= new Scanner(System.in);
-        String inputt= derf.nextLine();
-        while(!inputt.equals("1")&&!inputt.equals("2"))
+        Scanner derp= new Scanner(System.in);
+        String inputt= derp.nextLine();
+        String error="The string was either not a valid input or was neither 1 nor 2.";
+        while (readable(inputt).equals(error))
         {
-            System.out.println("Please type either 1 for Cave 1 or 2 for Cave 2.");
-            inputt=derf.nextLine();
+
+            System.out.println(readable(inputt));
+            inputt= derp.nextLine();
         }
         if(inputt.equals("1"))
         {
@@ -27,8 +29,23 @@ public class Dragon_Main {
             System.out.println("You enter a cave...\n" +
                     "It has a pleasant warmth to it...\n"+
             "You see a peacefully sleeping dragon on top of a bed of saffron flowers...\n"+
-            "The dragon awakens, and invites you to his stash of the finest saffron pistils the world has ever known."+
+            "The dragon awakens, and invites you to his stash of the finest saffron pistils the world has ever known..."+
                     "\n60 years later, you die a rich, satisfied man.");
         }
     }
-}
+    public static Object readable(String str)
+    {
+        try{
+            if (str.equals("1")||str.equals("2"))
+            {
+                return str;
+            }
+            else return "The string was either not a valid input or was neither 1 nor 2.";
+        }
+        catch(Exception e){
+            return "The string was either not a valid input or was neither 1 nor 2.";
+        }
+    }
+    }
+
+
